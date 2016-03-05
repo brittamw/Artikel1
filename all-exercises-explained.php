ade32f54714fa8afe5cee20ce371586873928a96
brittamw <britta-m-walter@t-online.de>
2016-03-05 14:08:20 +0100

    Added files via upload

---
+++ all-exercises-explained.php
@@ -0,0 +1,103 @@
+<?php 	require_once 'include/init.php.inc';
+
+/*Ab Bikes
+Bent Leg Sit Ups
+Bent Jackknifes
+Bent Leg Raises
+Burpees
+Commandos
+Dumbell Squat Press
+Decline Push Ups
+Drop Push Ups
+Jump Squats
+Jump Lunges
+Knee Ups
+Lay Down Push Ups
+Leg Raise and Hip Lift
+Mountain Climbers
+Medicine Ball Crab Walks
+Mountain Climbers and Shoulder Taps
+Push Ups
+Plank
+Reverse Lunge
+Snap Jumps
+Straight Leg Sit Up
+Straight leg Raises
+straight Leg Jackknifes
+Squats
+Sumo Squat
+Step Ups
+Stationary Lunges
+Sumo Jump Squats
+Split Squats
+Skipping
+Toe Taps
+Tricep Dips
+Tricep Dips Feet Raised
+Tuck Jumps
+walking Lunges
+Weighted Bent Leg Jackknifes
+Weighted Knee Ups
+X-Hops
+180 Degree Jump Squats
+
+*/
+	require_once 'include/header.html.inc';
+?>
+<div>
+	<h1>Bist du noch Traingsanfänger?</h1>
+	<h2>Schau dir erstmal alle Übungen mit Bild und Erklärung an:</h2>
+	
+	<li><a href="180-degree-jump-squats.php">180 Degree Jump Squats</a></li>
+	<li><a href="ab-bikes.php">Ab Bikes</a></li>
+	<li><a href="bent-jackknifes.php">Bent Jackknifes</a></li>
+	<li><a href="bent-leg-raises.php">Bent Leg Raises</a></li>
+	<li><a href="bent-leg-sit-ups.php">Bent Leg Sit Ups</a></li>
+	<li><a href="burpees.php">Burpees</a></li>
+	<li><a href="commandos.php">Commandos</a></li>
+	<li><a href="decline-push-ups.php">Decline Push Ups</a></li>
+	<li><a href="drop-push-ups.php">Drop Push Ups</a></li>
+	<li><a href="dumbbell-squat-press.php">Dumbbell Squat Press</a></li>
+	<li><a href="jump-lunges.php">Jump Lunges</a></li>
+	<li><a href="jump-squats.php">Jump Squats</a></li>
+	<li><a href="knee-ups.php">Knee Ups</a></li>
+	<li><a href="lay-down-push-ups.php">Lay Down Push Ups</a></li>
+	<li><a href="leg-raise-and-hip-lift.php">Leg Raise and Hip Lift</a></li>
+	<li><a href="medicine-ball-crab-walk.php">Medicine Ball Crab Walk</a></li>
+	<li><a href="mountain-climber-and-shoulder-taps.php">Mountain Climber and Shoulder Taps</a></li>
+	<li><a href="mountain-climbers.php">Mountain Climbers</a></li>
+	<li><a href="plank.php">Plank</a></li>
+	<li><a href="push-ups.php">Push Ups</a></li>
+	<li><a href="reverse-lunge.php">Reverse Lunge</a></li>
+	<li><a href="skipping.php">Skipping</a></li>
+	<li><a href="snap-jumps.php">Snap Jumps</a></li>
+	<li><a href="split-squats.php">Split Squats</a></li>
+	<li><a href="squats.php">Squats</a></li>
+	<li><a href="stationary-lunges.php">Stationary Lunges</a></li>
+	<li><a href="step-ups.php">Step Ups</a></li>
+	<li><a href="straight-leg-jackknifes.php">Straight Leg Jackknifes</a></li>
+	<li><a href="straight-leg-raises.php">Straight Leg Raises</a></li>
+	<li><a href="straight-leg-sit-ups.php">Straight Leg Sit Ups</a></li>
+	<li><a href="sumo-jump-squats.php">Sumo Jump Squats</a></li>
+	<li><a href="sumo-squat.php">Sumo Squats</a></li>
+	<li><a href="toe-taps.php">Toe Taps</a></li>
+	<li><a href="tricep-dips.php">Tricep Dips</a></li>
+	<li><a href="tricep-dips-feet-raised.php">Tricep Dips Feet Raises</a></li>
+	<li><a href="tuck-jumps.php">Tuck Jumps</a></li>
+	<li><a href="walking-lunges.php">Walking Lunges</a></li>
+	<li><a href="weighted-bent-leg-jackknifes.php">Weighted Bent Leg Jackknifes</a></li>
+	<li><a href="weighted-knee-ups.php">Weighted Knee Ups</a></li>
+	<li><a href="x-hops.php">X-Hops</a></li>
+	
+	
+	
+	
+	
+	
+
+
+</div>
+
+<?php
+	require_once 'include/footer.html.inc';
+?>
